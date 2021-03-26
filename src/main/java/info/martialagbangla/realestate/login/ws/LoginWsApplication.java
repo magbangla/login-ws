d300b0c649566ff8ea63ea5f7ee0c424a0dc9a0d
martial <amour.martial@gmail.com>
2021-03-26 02:46:21 -0400

    initial commit login ws

---
+++ src/main/java/info/martialagbangla/realestate/login/ws/LoginWsApplication.java
@@ -0,0 +1,13 @@
+package info.martialagbangla.realestate.login.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/martialagbangla/realestate/login/ws/LoginWsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.martialagbangla.realestate.login.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
